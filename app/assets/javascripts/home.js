a222441caa9f7929fa22a8b9da6dda839a5005ef
Hans-Sebastian <hanssebastian.p@gmail.com>
2019-02-10 17:36:17 -0500

    hover change html

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,37 +2,35 @@
 $(document).ready(function(){ 
   var button1 = $('#button1');
   var bio1 = $('#bio1');
+  var bio1A = $('#bio1A');
   var button2 = $('#button2');
   var bio2 = $('#bio2');
-  var goto1 = $('#goto1')
-  var goto2 = $('#goto2')  
+  var goto1 = $('#goto1');
+  var goto2 = $('#goto2');
+  var trumpet = $('#trumpet');
 
-  $(function() {
-    var timeoutId;
+  var TrumpetPath = "https://s3.amazonaws.com/denizilhan/trumpetbw.png";
+  var ColorTrumpetPath = "https://s3.amazonaws.com/denizilhan/trumpetcolor4.png";
+
+
     button1.hover(function() {
-        if (!timeoutId) {
-            timeoutId = window.setTimeout(function() {
-                timeoutId = null;
-                bio2.hide();
+
+                bio1A.text("Bio Numero Uno. Short bio for each page and what it contains");
+                goto1.text("Go To Sketches");
+                goto1.href = "#"
                 button2.css('box-shadow', 'none');
                 button1.css('box-shadow', '0px 5px 20px -5px rgba(0,0,0,.5)');
-                bio1.fadeIn(1200);
-           }, 250);
-        }
+                trumpet.attr('src', TrumpetPath);
+
     });
-  });
-  $(function() {
-    var timeoutId;
     button2.hover(function() {
-        if (!timeoutId) {
-            timeoutId = window.setTimeout(function() {
-                timeoutId = null;
-                bio1.hide();
+
+                bio1A.text("Bio Numero Dos. Short bio for each page and what it contains");
+                goto1.text("Go To Animations");
+                goto1.href = "#"
                 button1.css('box-shadow', 'none');
                 button2.css('box-shadow', '0px 5px 20px -5px rgba(0,0,0,.5)');
-                bio2.fadeIn(1200);
-           }, 250);
-        }
-    });
+                trumpet.attr('src', ColorTrumpetPath);
+
   });
 });
